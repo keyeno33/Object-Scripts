509115074c01e75599f69b4ce3eacb7b5ef28827
keyeno33 <robert.299@hotmail.com>
2016-05-27 23:59:52 -0400

    Functional Object scripts
    Scripts for functional game objects

---
+++ PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public int speed;
+
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+    // Update is called once per frame
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moverVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moverVertical);
+        rb.AddForce(movement * speed);
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Speed_Cube")
+        {
+
+            Destroy(col.gameObject);
+            speed = 20;
+        }
+        else if (col.gameObject.name == "Dec_Speed_cube")
+        {
+            Destroy(col.gameObject);
+            speed -= 5;
+        }
+    }
+}
---
+++ increaseSize.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class increaseSize : MonoBehaviour {
+    public int size;
+  
+	// Use this for initialization
+	void Start () {
+        Transfrom.Scale(size, size, size);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+}
