0a33a85c00505eece49e1bb4fccf27f1827add2f
keyeno33 <robert.299@hotmail.com>
2016-05-28 22:35:26 -0400

    Added new functions

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,14 +4,22 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public int speed;
+    public string ObjSpeedIncr = "Speed_Cube";
+    public string ObjSpeedDec = "Dec_Speed_Cube";
+    public string ObjScaleinc = "Scale_Inc";
+    public string ObjScaleDec = "Scale_Dc";
 
     private Rigidbody rb;
-	// Use this for initialization
+	
+    // Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
 	}
+
     // Update is called once per frame
     void FixedUpdate () {
+        
+        //Get input for each axis;
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moverVertical = Input.GetAxis ("Vertical");
 
@@ -20,16 +28,30 @@ public class PlayerController : MonoBehaviour {
 	}
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name == "Speed_Cube")
-        {
+        //initial state of collision 
+        bool collisionState = false;
 
-            Destroy(col.gameObject);
-            speed = 20;
-        }
-        else if (col.gameObject.name == "Dec_Speed_cube")
+        //switch case for each object collision
+        switch(collisionState)
         {
-            Destroy(col.gameObject);
-            speed -= 5;
+            case gameObject.name == ObjSpeedIncr:
+                    Destroy(col.gameObject);
+                    speed = 20;
+                break;
+
+            case gameObject.name == ObjSpeedIncr:
+                    Destroy(col.gameObject);
+                    speed -= 5;
+                break;
+
+            case gameObject.name == ObjScaleinc:
+
+            break;
+
+            case gameObject.name == ObjScaleDec:
+                break;
+                  
+            default:
+                return collisionState;
         }
-    }
 }
